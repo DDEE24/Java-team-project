e82b7a587290203cfc6c7f23c02c55b4d973a63a
TastyFuji <ajizflame@gmail.com>
2025-07-29 02:53:15 +0700

    Change textPane

--- src/ChatUI.java
+++ src/ChatUI.java
@@ -1,13 +1,57 @@
 import javax.swing.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Style;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ChatUI {
     private JPanel Body;
     private JTextField inputText;
     private JButton sendButton;
-    private JTextArea textArea;
+    private JTextPane textPane;
 
+
+    public ChatUI() {
+
+        inputText.addActionListener(e -> sendButton.doClick());
+
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String text = inputText.getText();
+                appendTitle("User : \n", Color.CYAN);
+                appendWithColor("  " + text + "\n", Color.WHITE);
+                inputText.setText("");
+            }
+        });
+    }
 
     public JPanel getBody() {
         return Body;
+    }
+
+    private void appendTitle(String text, Color color) {
+        StyledDocument doc = textPane.getStyledDocument();
+        Style style = textPane.addStyle("Color Style", null);
+        StyleConstants.setForeground(style, color);
+        StyleConstants.setBold(style, true);
+        try {
+            doc.insertString(doc.getLength(), text, style);
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+    }
+    private void appendWithColor(String text, Color color) {
+        StyledDocument doc = textPane.getStyledDocument();
+        Style style = textPane.addStyle("Color Style", null);
+        StyleConstants.setForeground(style, color);
+        try {
+            doc.insertString(doc.getLength(), text, style);
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
     }
 }
